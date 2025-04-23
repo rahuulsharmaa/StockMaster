fb4114aa64a864ffc0f87b4ba1440fbe4b9ff349
rahuulsharmaa <rahul26580058@gmail.com>
2025-04-23 23:16:23 +0530

    connetion.py

--- database/connection.py
+++ database/connection.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# PostgreSQL connection string
-# Format: postgresql://username:password@host:port/database_name
+
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:2658@localhost:5432/stockmaster_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
